c6ed3e800bde08fa5611778aee697ff64aeddde6
Kanchan Srivastava <kanchansrivastava1991@gmail.com>
2019-07-18 09:37:38 +0530

    Rectangle Practice

---
+++ src/Rectangle.java
@@ -0,0 +1,26 @@
+public class Rectangle {
+    int length;
+    int breadth;
+
+    public Rectangle (int length, int breadth){
+        this.length = length;
+        this.breadth = breadth;
+
+    }
+
+    public Rectangle(int length){
+        this.length = length;
+        this.breadth = length;
+    }
+
+    public int area() {
+        return length * breadth;
+    }
+
+    public int perimeter() {
+        return 2 * (length + breadth);
+    }
+
+}
+
+
---
+++ test/RectangleTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RectangleTest {
+
+    @Test
+    public void area_shouldReturn8_whenGivenLengthIs4AndBreadthIs2(){
+        Rectangle rectangle = new Rectangle(4, 2);
+        int result = rectangle.area();
+        assertEquals(8, result);
+    }
+
+    @Test
+    public void perimeter_shouldReturn12_whenGivenLengthIs4AndBreadthIs2(){
+        Rectangle rectangle = new Rectangle(4, 2);
+        int result = rectangle.perimeter();
+        assertEquals(12, result);
+    }
+
+    @Test
+    public void area_shouldReturn16_whenGivenLengthIs4(){
+        Rectangle square = new Rectangle(4);
+        int result = square.area();
+        assertEquals(16, result);
+    }
+
+}
+
